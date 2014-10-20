6dbd6600a8989e8aefd577ac24501dff1657f932
hero <hero@asterisk-works.jp>
2014-10-21 00:31:38 +0900

    RSpec のひな形を追加

---
+++ spec/tl_capture_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe TlCapture do
+  it 'has a version number' do
+    expect(TlCapture::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
